093dbcf27ba2905a09630dd75306e9d3ba9b4077
Pierre-Antoine <97026094+PAAMIANDLEROY@users.noreply.github.com>
2025-01-07 17:33:21 +0100

    Add files via upload

---
+++ Dataset_generation.py
@@ -0,0 +1,75 @@
+import scipy.stats as stat
+import numpy as np
+
+
+######Multivariate symmetric logistic Model
+####see "Simulating Multivariate Extreme Value Distributions of Logistic Type", 2003, A. Stephenson for more details
+
+
+def gen_PositiveStable(n,alpha,seed=np.random.randint(2**31)): # From Nathan Huet
+    """
+    Generate positive stable random variables.
+
+    Inputs:
+    - n : sample size
+    - alpha : dependence parameter
+    - seed : random seed
+
+    Output:
+    - Sample of positive stable random variables
+    """
+    U=stat.uniform.rvs(0,np.pi,size=n,random_state = seed)
+    W=stat.expon.rvs(size=n,random_state = seed)
+    Term_1=(np.sin((1-alpha)*U)/W)**((1-alpha)/alpha)
+    Term_2=np.sin(alpha*U)/((np.sin(U))**(1/alpha))
+    return Term_1*Term_2
+
+
+def gen_multilog(n,Dim,alpha,Hill_index,seed=np.random.randint(2**31)):  # From Nathan Huet
+    #Assymetric !!!!!!!!!!!! ALgo 2.1 dans le papier 
+    
+    """
+    Generate multivariate logistic random variables.  
+
+    Inputs:
+    - n : sample size
+    - Dim : dimension
+    - alpha : dependence parameter
+    - Hill_index : Hill index
+    - seed : random seed
+
+    Output:
+    - Sample of multivariate logistic random variables
+    """
+    W=stat.expon.rvs(size=(n,Dim),random_state = seed) ### Standard exponential Random Variables
+
+    S=gen_PositiveStable(n,alpha,seed)### TO check
+    Log=np.zeros((n,Dim))
+    for ii in range(Dim):
+        Log[:,ii]=(S/W[:,ii])**alpha
+    return np.clip(Log**(1/Hill_index),a_min=0,a_max=1e3)
+
+def gen_label(Matrix,angle=0.2): #PA
+    #Gen label 2D
+    Vect_angle=np.arctan2(Matrix[:,1],Matrix[:,0])
+    label=[0 if (point_angle<(angle*np.pi/2) or (point_angle>((1-angle)*np.pi/2))) else 1 for point_angle in Vect_angle]
+    return np.array(label)
+
+
+def gen_label2(Matrix,angle=0.2): #PA
+    #Gen label 2D
+    Vect_angle=np.arctan2(Matrix[:,1],Matrix[:,0])
+    label = [
+        0 if (
+            (0 <= point_angle < angle * np.pi / 4) or
+            (1 - angle) * np.pi / 4 <= point_angle < np.pi / 4 or
+            np.pi / 4 + angle * np.pi / 4 <= point_angle < np.pi / 4 + (1 - angle) * np.pi / 4
+        ) else 1
+        for point_angle in Vect_angle
+    ]
+    return np.array(label)
+
+
+
+
+
