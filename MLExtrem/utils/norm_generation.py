761f572d0edb5324e0a15a40349a3e59351e362e
Pierre-Antoine <97026094+PAAMIANDLEROY@users.noreply.github.com>
2025-01-15 16:01:23 +0100

    Create norm_generation.py

---
+++ MLExtrem/utils/norm_generation.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+
+def norm_L1(vector):
+    return np.sum(np.abs(vector))
+    
+def norm_L2(vector):
+    return np.sqrt(np.sum(vector**2))
+
+def norm_Linf(vector):
+    return np.max(np.abs(vector))
+
+def norm_L3(vector):
+    return np.sum(np.abs(vector)**3)**(1/3)
+
+def norm_L0(vector):
+    return np.sum(vector != 0)
+    
