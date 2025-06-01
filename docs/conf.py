cf9fb436ef690630a92477354d6adf5cbc814be2
Pierre-Antoine <97026094+PAAMIANDLEROY@users.noreply.github.com>
2025-06-01 23:21:24 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -4,6 +4,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../MLExtreme'))
+sys.path.insert(0, os.path.abspath('..'))
 
 
 # For the full list of built-in configuration values, see the documentation:
